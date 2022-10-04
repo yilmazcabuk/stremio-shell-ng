85f7223c39572f23acfd291aceaa4523a0b5fed0
Vladimir Borisov <borisov1024@gmail.com>
2022-10-04 14:08:58 +0300

    Embedded metadata

--- build.rs
+++ build.rs
@@ -1,4 +1,16 @@
-extern crate embed_resource;
+use chrono::{Datelike, Local};
+use std::env;
+
+extern crate winres;
 fn main() {
-    embed_resource::compile("resources.rc");
+    let now = Local::now();
+    let copyright = format!("Copyright © {} Smart Code OOD", now.year());
+    let exe_name = format!("{}.exe", env::var("CARGO_PKG_NAME").unwrap());
+    let mut res = winres::WindowsResource::new();
+    res.set("FileDescription", "Freedom to Stream");
+    res.set("LegalCopyright", &copyright);
+    res.set("OriginalFilename", &exe_name);
+    res.set_icon_with_id("images/stremio.ico", "MAINICON");
+    res.append_rc_content(r##"SPLASHIMAGE IMAGE "images/stremio.png""##);
+    res.compile().unwrap();
 }
